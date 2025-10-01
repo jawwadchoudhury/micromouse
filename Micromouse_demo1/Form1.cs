284fb13d24577e5e3f79d1673cbab8b7fa9c9178
Jawwad <69754686+jawwadchoudhury@users.noreply.github.com>
2025-10-01 10:09:40 +0100

    Update Form1.cs

--- Micromouse_demo1/Form1.cs
+++ Micromouse_demo1/Form1.cs
@@ -12,6 +12,8 @@ namespace Micromouse_demo1
 {
     public partial class Form1 : Form
     {
+        
+        private int[] mazeArray = {3, 3, 2, 6, 3, 14, 4, 4, 1, 8, 8};
         public Form1()
         {
             InitializeComponent();
@@ -137,10 +139,10 @@ namespace Micromouse_demo1
             {
                 points = new Point[]
                 {
+                    new Point(0, bs),
                     new Point(0, 0),
                     new Point(bs, 0),
-                    new Point(bs, bs),
-                    new Point(0, bs)
+                    new Point(bs, bs)
                 };
             }
             else if (i == 12)
@@ -158,6 +160,26 @@ namespace Micromouse_demo1
                 points = new Point[]
                 {
                     new Point(0, 0),
+                    new Point(bs + 10, 0),
+                    new Point(bs + 10, bs),
+                    new Point(0, bs)
+                };
+            }
+            else if (i == 14)
+            {
+                points = new Point[]
+                {
+                    new Point(0, 0),
+                    new Point(0, bs + 10),
+                    new Point(bs, bs + 10),
+                    new Point(bs, 0)
+                };
+            }
+            else if (i == 15)
+            {
+                points = new Point[]
+                {
+                    new Point(0, 0),
                     new Point(bs, 0),
                     new Point(bs, bs),
                     new Point(0, bs),
@@ -167,31 +189,78 @@ namespace Micromouse_demo1
             return points;
         }
 
+        int j;
+        int BoxSize = 50;
         private void pictureBox_Paint(object sender, PaintEventArgs e)
         {
             e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-            Lines lines = new Lines(shapePoints(j, boxSize / 2), Color.Black, 3);
+            Lines lines = new Lines(shapePoints(j, BoxSize), Color.Black, 5);
             lines.Draw(e.Graphics);
             j++;
         }
-
-        int i = 0;
-        int j = 1;
-        int x = 10;
-        int y = 10;
-        int boxSize = 50;
+       
         private void Form1_Load(object sender, EventArgs e)
         {
-            for (int i = 0; i <= 12; i++)
+            generateMaze(mazeArray, 50);
+            //generateAllIcons(10, 10, 50);
+
+            //Console.Write("Maze height: ");
+            //int.TryParse(Console.ReadLine(), out mazeHeight);
+            //Console.Write("Maze width: ");
+            //int.TryParse(Console.ReadLine(), out mazeWidth);
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write($"Enter number {i}/{mazeHeight * mazeWidth}: ");
+            //}
+        }
+
+        int x;
+        private void generateAllIcons(int startX, int startY, int boxSize)
+        {
+            j = 1;
+            x = startX;
+            for (int i = 0; i < 15; i++)
             {
                 PictureBox pictureBox = new PictureBox();
                 pictureBox.Name = $"pictureBox_{i}";
                 pictureBox.Size = new Size(boxSize, boxSize);
-                pictureBox.Location = new Point(x, y);
-                x = x + boxSize;
+                pictureBox.Location = new Point(x, startY);
+                x = x + boxSize + 10;
                 this.Controls.Add(pictureBox);
                 pictureBox.Paint += new PaintEventHandler(pictureBox_Paint);
             }
         }
+
+        private void generateMaze(int[] maze, int boxSize)
+        {
+            int x = 10;
+            int y = 50;
+            mazeArrayInt = 2;
+            for (int i = 0; i < maze[0]; i++)
+            {
+                for (int j = 0; j < maze[1]; j++)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBox.Name = $"pictureBox_{i}";
+                    pictureBox.Size = new Size(boxSize, boxSize);
+                    pictureBox.Location = new Point(x, y);
+                    x = x + boxSize;
+                    this.Controls.Add(pictureBox);
+                    pictureBox.Paint += new PaintEventHandler(mazeBox_paint);
+                };
+                x = 10;
+                y = y + boxSize;
+            };
+        }
+
+        int mazeArrayInt;
+        private void mazeBox_paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Lines lines = new Lines(shapePoints(mazeArray[mazeArrayInt], BoxSize), Color.Black, 5);
+            lines.Draw(e.Graphics);
+            mazeArrayInt++;
+        }
+
     }
 }
