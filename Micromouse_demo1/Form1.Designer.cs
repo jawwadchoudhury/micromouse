852837af1056232deb8c5323dc86449ceb153582
Jawwad Choudhury <244680@esher.ac.uk>
2025-11-30 17:40:58 +0000

    Implemented breadth-first search

--- Micromouse_demo1/Form1.Designer.cs
+++ Micromouse_demo1/Form1.Designer.cs
@@ -46,56 +46,61 @@
             // 
             this.outputLabel1.AutoSize = true;
             this.outputLabel1.Location = new System.Drawing.Point(0, 0);
-            this.outputLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.outputLabel1.Name = "outputLabel1";
-            this.outputLabel1.Size = new System.Drawing.Size(103, 20);
+            this.outputLabel1.Size = new System.Drawing.Size(69, 13);
             this.outputLabel1.TabIndex = 0;
             this.outputLabel1.Text = "outputLabel1";
             // 
             // mazePanel
             // 
-            this.mazePanel.Location = new System.Drawing.Point(4, 63);
+            this.mazePanel.Location = new System.Drawing.Point(3, 41);
+            this.mazePanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.mazePanel.Name = "mazePanel";
-            this.mazePanel.Size = new System.Drawing.Size(600, 615);
+            this.mazePanel.Size = new System.Drawing.Size(400, 400);
             this.mazePanel.TabIndex = 6;
             // 
             // newMazeHeight_Label
             // 
             this.newMazeHeight_Label.AutoSize = true;
-            this.newMazeHeight_Label.Location = new System.Drawing.Point(20, 838);
+            this.newMazeHeight_Label.Location = new System.Drawing.Point(13, 545);
+            this.newMazeHeight_Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.newMazeHeight_Label.Name = "newMazeHeight_Label";
-            this.newMazeHeight_Label.Size = new System.Drawing.Size(60, 20);
+            this.newMazeHeight_Label.Size = new System.Drawing.Size(41, 13);
             this.newMazeHeight_Label.TabIndex = 11;
             this.newMazeHeight_Label.Text = "Height:";
             // 
             // newMazeWidth_Label
             // 
             this.newMazeWidth_Label.AutoSize = true;
-            this.newMazeWidth_Label.Location = new System.Drawing.Point(26, 803);
+            this.newMazeWidth_Label.Location = new System.Drawing.Point(17, 522);
+            this.newMazeWidth_Label.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.newMazeWidth_Label.Name = "newMazeWidth_Label";
-            this.newMazeWidth_Label.Size = new System.Drawing.Size(54, 20);
+            this.newMazeWidth_Label.Size = new System.Drawing.Size(38, 13);
             this.newMazeWidth_Label.TabIndex = 10;
             this.newMazeWidth_Label.Text = "Width:";
             // 
             // newMaze_width
             // 
-            this.newMaze_width.Location = new System.Drawing.Point(94, 800);
+            this.newMaze_width.Location = new System.Drawing.Point(63, 520);
+            this.newMaze_width.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.newMaze_width.Name = "newMaze_width";
-            this.newMaze_width.Size = new System.Drawing.Size(163, 26);
+            this.newMaze_width.Size = new System.Drawing.Size(110, 20);
             this.newMaze_width.TabIndex = 9;
             // 
             // newMaze_height
             // 
-            this.newMaze_height.Location = new System.Drawing.Point(94, 832);
+            this.newMaze_height.Location = new System.Drawing.Point(63, 541);
+            this.newMaze_height.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.newMaze_height.Name = "newMaze_height";
-            this.newMaze_height.Size = new System.Drawing.Size(163, 26);
+            this.newMaze_height.Size = new System.Drawing.Size(110, 20);
             this.newMaze_height.TabIndex = 8;
             // 
             // generateNewMaze
             // 
-            this.generateNewMaze.Location = new System.Drawing.Point(94, 865);
+            this.generateNewMaze.Location = new System.Drawing.Point(63, 562);
+            this.generateNewMaze.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.generateNewMaze.Name = "generateNewMaze";
-            this.generateNewMaze.Size = new System.Drawing.Size(164, 52);
+            this.generateNewMaze.Size = new System.Drawing.Size(109, 34);
             this.generateNewMaze.TabIndex = 7;
             this.generateNewMaze.Text = "Generate New Maze";
             this.generateNewMaze.UseVisualStyleBackColor = true;
@@ -103,9 +108,10 @@
             // 
             // prims_Button
             // 
-            this.prims_Button.Location = new System.Drawing.Point(266, 865);
+            this.prims_Button.Location = new System.Drawing.Point(177, 562);
+            this.prims_Button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.prims_Button.Name = "prims_Button";
-            this.prims_Button.Size = new System.Drawing.Size(164, 52);
+            this.prims_Button.Size = new System.Drawing.Size(109, 34);
             this.prims_Button.TabIndex = 12;
             this.prims_Button.Text = "Run Prim\'s Algorithm";
             this.prims_Button.UseVisualStyleBackColor = true;
@@ -114,48 +120,44 @@
             // primaryNodeLabel
             // 
             this.primaryNodeLabel.AutoSize = true;
-            this.primaryNodeLabel.Location = new System.Drawing.Point(20, 757);
-            this.primaryNodeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.primaryNodeLabel.Location = new System.Drawing.Point(13, 492);
             this.primaryNodeLabel.Name = "primaryNodeLabel";
-            this.primaryNodeLabel.Size = new System.Drawing.Size(115, 20);
+            this.primaryNodeLabel.Size = new System.Drawing.Size(78, 13);
             this.primaryNodeLabel.TabIndex = 13;
             this.primaryNodeLabel.Text = "Primary Nodes:";
             // 
             // frontierNodeLabel
             // 
             this.frontierNodeLabel.AutoSize = true;
-            this.frontierNodeLabel.Location = new System.Drawing.Point(18, 777);
-            this.frontierNodeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.frontierNodeLabel.Location = new System.Drawing.Point(12, 505);
             this.frontierNodeLabel.Name = "frontierNodeLabel";
-            this.frontierNodeLabel.Size = new System.Drawing.Size(118, 20);
+            this.frontierNodeLabel.Size = new System.Drawing.Size(79, 13);
             this.frontierNodeLabel.TabIndex = 14;
             this.frontierNodeLabel.Text = "Frontier Nodes:";
             // 
             // outputLabel2
             // 
             this.outputLabel2.AutoSize = true;
-            this.outputLabel2.Location = new System.Drawing.Point(1, 22);
-            this.outputLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputLabel2.Location = new System.Drawing.Point(1, 14);
             this.outputLabel2.Name = "outputLabel2";
-            this.outputLabel2.Size = new System.Drawing.Size(103, 20);
+            this.outputLabel2.Size = new System.Drawing.Size(69, 13);
             this.outputLabel2.TabIndex = 15;
             this.outputLabel2.Text = "outputLabel2";
             // 
             // outputLabel3
             // 
             this.outputLabel3.AutoSize = true;
-            this.outputLabel3.Location = new System.Drawing.Point(1, 42);
-            this.outputLabel3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.outputLabel3.Location = new System.Drawing.Point(1, 27);
             this.outputLabel3.Name = "outputLabel3";
-            this.outputLabel3.Size = new System.Drawing.Size(103, 20);
+            this.outputLabel3.Size = new System.Drawing.Size(69, 13);
             this.outputLabel3.TabIndex = 16;
             this.outputLabel3.Text = "outputLabel3";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(978, 945);
+            this.ClientSize = new System.Drawing.Size(652, 614);
             this.Controls.Add(this.outputLabel3);
             this.Controls.Add(this.outputLabel2);
             this.Controls.Add(this.frontierNodeLabel);
@@ -168,6 +170,7 @@
             this.Controls.Add(this.generateNewMaze);
             this.Controls.Add(this.mazePanel);
             this.Controls.Add(this.outputLabel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
--- Micromouse_demo1/Form1.cs
+++ Micromouse_demo1/Form1.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Micromouse_demo1
 {
@@ -44,6 +46,9 @@ namespace Micromouse_demo1
         }
         public class Node
         {
+            public bool isEndNode { get; set; } = false;
+            public bool isStartNode { get; set; } = false;
+            public bool isVisited { get; set; } = false;
             public int DecimalValue { get; private set; }
             //T, R, B, L
             //1111 is a rectangle
@@ -113,7 +118,34 @@ namespace Micromouse_demo1
             public void setDecimalValue(int decimalValue)
             {
                 DecimalValue = decimalValue;
-            } 
+            }
+            private int getWallCount()
+            {
+                int wallCount = 0;
+                if (getTopValue()) wallCount++;
+                if (getRightValue()) wallCount++;
+                if (getBottomValue()) wallCount++;
+                if (getLeftValue()) wallCount++;
+                return wallCount;
+            }
+            public bool isKeyNode()
+            {
+                int wallCount = getWallCount();
+                if (isStartNode || isEndNode || wallCount <= 1 || wallCount >= 3) return true;
+                if (wallCount == 2)
+                {
+                    // Check if the two walls are opposite each other
+                    if ((getTopValue() && getBottomValue()) || (getRightValue() && getLeftValue()))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
         }
         public class MazeNode : Node
         {
@@ -182,35 +214,27 @@ namespace Micromouse_demo1
             // This is stupid so i have a better idea! Weighted adjacency matrix. Get a node, go straight in each direction till you hit a wall, add that weight into the adjacency matrix,
             // do this for all 4 directions, then iterate over each one of them, kind of like the frontier thing again
             // we'll see how that goes yeah
-            private int StartingIndex;
-            private Dictionary<int, List<int[]>> adjacencyDictionary;
-            public WeightedAdjacencyMatrix(int startingIndex)
+            private Dictionary<int, List<int[]>> adjacencyDictionary = new Dictionary<int, List<int[]>>();
+            public WeightedAdjacencyMatrix()
             {
-                StartingIndex = startingIndex;
             }
 
-            //public void addValue(int nodeIndex, int adjacentNodeIndex, int weight)
-            //{
-            //    int[] valuePair = new int[2] {adjacentNodeIndex, weight};
-            //    if (adjacencyDictionary.TryGetValue(nodeIndex, out var weightedValues)) {
-            //        if (!weightedValues.Contains(valuePair))
-            //        {
-            //            weightedValues.Add(valuePair);
-            //            adjacencyDictionary.Add(nodeIndex, weightedValues);
-            //        }
-            //    } else
-            //    {
-            //        adjacencyDictionary.Add(nodeIndex, new List<int[]> { new int[] { adjacentNodeIndex, weight } });
-            //    }
-            //}
-            public string test()
+            public void addValue(int nodeIndex, int adjacentNodeIndex, int weight)
             {
-                if (adjacencyDictionary.TryGetValue(0, out var list))
+                int[] valuePair = new int[2] { adjacentNodeIndex, weight };
+                if (adjacencyDictionary.TryGetValue(nodeIndex, out var weightedValues))
                 {
-                    return list.ToString();
-                } else
+                    if (!weightedValues.Contains(valuePair))
+                    {
+                        weightedValues.Add(valuePair);
+                        adjacencyDictionary[nodeIndex] = weightedValues;
+                        Console.WriteLine($"Added value pair: Node {nodeIndex} -> Adjacent Node {adjacentNodeIndex} with weight {weight}");
+                    }
+                }
+                else
                 {
-                    return "error...";
+                    adjacencyDictionary.Add(nodeIndex, new List<int[]> { new int[] { adjacentNodeIndex, weight } });
+                    Console.WriteLine($"Added value pair: Node {nodeIndex} -> Adjacent Node {adjacentNodeIndex} with weight {weight}");
                 }
             }
         }
@@ -218,11 +242,9 @@ namespace Micromouse_demo1
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
         }
-
         private void clearAll()
         {
             mazePanel.Controls.Clear();
@@ -232,10 +254,11 @@ namespace Micromouse_demo1
             currentMazeData = new int[6];
             frontierNodes.Clear();
             primaryNodes.Clear();
+            weightedAdjacencyMatrix = null;
             primaryNodeLabel.Text = "Primary Nodes: ";
             frontierNodeLabel.Text = "Frontier Nodes: ";
         }
-        private void createEmptyMaze(int width, int height,  int boxSize)
+        private void createEmptyMaze(int width, int height, int boxSize)
         {
             currentMaze.Clear();
             currentMazeData[0] = width;
@@ -343,6 +366,11 @@ namespace Micromouse_demo1
         }
         private async void primsAlgorithm(int startingNode)
         {
+            if (currentMaze.Count == 0)
+            {
+                MessageBox.Show("Please create a maze first.");
+                return;
+            }
             primaryNodes.Add(startingNode); // Turn the starting node into a primary node
             refreshLabels();
 
@@ -351,7 +379,7 @@ namespace Micromouse_demo1
             // Iterate tunnel function
             while (frontierNodes.Count > 0)
             {
-                await Task.Delay(20);
+                await Task.Delay(2);
                 List<int> frontierAdjacentNodes;
                 int frontierNodeIndex = frontierNodes[random.Next(0, frontierNodes.Count)];
                 adjacencyMatrix.TryGetValue(frontierNodeIndex, out frontierAdjacentNodes);
@@ -372,7 +400,7 @@ namespace Micromouse_demo1
                 case 0:
                     currentMazeData[4] = random.Next(0, currentMazeData[0]);
                     currentMaze[currentMazeData[4]].removeTopValue();
-                    
+
                     break;
                 case 1:
                     List<int> rightEdges = new List<int>();
@@ -397,17 +425,10 @@ namespace Micromouse_demo1
                     currentMaze[currentMazeData[4]].removeLeftValue();
                     break;
             }
+            currentMaze[currentMazeData[4]].isEndNode = true;
 
             drawCurrentMaze(); // Redraw the maze (without the weird buggy lines)
         }
-
-        private async void graphGenerationAlgorithm(int startingNode)
-        {
-            graphGenerationStarted = true;
-            weightedAdjacencyMatrix = new WeightedAdjacencyMatrix(startingNode);
-            weightedAdjacencyMatrix.test();
-        }
-
         private void createTunnel(int startIndex, int endIndex)
         {
             returnFrontierNodes(currentMaze, endIndex);
@@ -437,7 +458,6 @@ namespace Micromouse_demo1
                     break;
             }
         }
-
         private int getDirection(int startIndex, int endIndex)
         {
             MazeNode startNode = currentMaze[startIndex];
@@ -449,7 +469,6 @@ namespace Micromouse_demo1
             if (startIndex - 1 == endIndex) return 3; // Left
             return -1;
         }
-
         private void returnFrontierNodes(List<MazeNode> maze, int nodeIndex)
         {
             MazeNode centralNode = maze[nodeIndex];
@@ -472,7 +491,6 @@ namespace Micromouse_demo1
             if (leftNode != null && !primaryNodes.Contains(leftNode.NodeIndex) && !frontierNodes.Contains(leftNode.NodeIndex)) frontierNodes.Add(leftNode.NodeIndex);
             refreshLabels();
         }
-
         private int[] returnAdjacentIndexes(int nodeIndex)
         {
             int[] adjacentIndexes = new int[4];
@@ -495,6 +513,181 @@ namespace Micromouse_demo1
             return adjacentIndexes;
         }
 
+        //private async void graphGenerationAlgorithm(int startingNode)
+        //{
+        //    currentMazeData[5] = startingNode;
+        //    currentMaze[startingNode].isStartNode = true;
+        //    graphGenerationStarted = true;
+        //    if (weightedAdjacencyMatrix == null) generateWeightedAdjacencyMatrix();
+
+        //}
+
+        //private void generateWeightedAdjacencyMatrix()
+        //{
+        //    weightedAdjacencyMatrix = new WeightedAdjacencyMatrix();
+        //    int weight = 0;
+        //    int parentNode = 0;
+        //    List<int> keyNodes = new List<int>();
+        //    for (int i = 0; i < currentMaze.Count; i++)
+        //    {
+        //        if (currentMaze[i].isKeyNode())
+        //        {
+        //            keyNodes.Add(i);
+        //            mazePanel.Controls[i].BackColor = Color.FromArgb(255, 155, 155);
+        //        }
+        //    }
+
+        //    foreach (int keyNodeIndex in keyNodes)
+        //    {
+        //        parentNode = keyNodeIndex;
+        //        int[] adjacentIndexes = returnAdjacentIndexes(keyNodeIndex);
+        //        // Up
+        //        weight = 0;
+        //        for (int j = adjacentIndexes[0]; j >= 0; j -= currentMazeData[0])
+        //        {
+        //            weight++;
+        //            if (currentMaze[j].isKeyNode())
+        //            {
+        //                weightedAdjacencyMatrix.addValue(parentNode, j, weight);
+        //                break;
+        //            }
+        //        }
+        //        // Right
+        //        weight = 0;
+        //        for (int j = adjacentIndexes[1]; j != -1; j += 1)
+        //        {
+        //            weight++;
+        //            if (currentMaze[j].isKeyNode())
+        //            {
+        //                weightedAdjacencyMatrix.addValue(parentNode, j, weight);
+        //                break;
+        //            }
+        //            if (currentMaze[j].WidthCount + 1 >= currentMazeData[0]) break;
+        //        }
+        //        // Down
+        //        weight = 0;
+        //        for (int j = adjacentIndexes[2]; j < currentMazeData[3]; j += currentMazeData[0])
+        //        {
+        //            weight++;
+        //            if (currentMaze[j].isKeyNode())
+        //            {
+        //                weightedAdjacencyMatrix.addValue(parentNode, j, weight);
+        //                break;
+        //            }
+        //        }
+        //        // Left
+        //        weight = 0;
+        //        for (int j = adjacentIndexes[3]; j != -1; j -= 1)
+        //        {
+        //            weight++;
+        //            if (currentMaze[j].isKeyNode())
+        //            {
+        //                weightedAdjacencyMatrix.addValue(parentNode, j, weight);
+        //                break;
+        //            }
+        //            if (currentMaze[j].WidthCount - 1 < 0) break;
+        //        }
+        //    }
+
+        //}
+        public List<int> BFS(int startNodeIndex, int endNodeIndex) // Breadth-First Search
+        {
+            Queue<int> queue = new Queue<int>(); // Nodes that need to be visited
+            queue.Enqueue(startNodeIndex); // Add start node index to the queue to begin with
+
+            Dictionary<int, int> parentMap = new Dictionary<int, int>(); // Keeps track of where you've been (child to parent)
+            parentMap.Add(startNodeIndex, -1); // -1 means there is no parent (start node)
+
+            HashSet<int> visited = new HashSet<int>(); // Keeps track of visited nodes (hashsets don't allow duplicates which is nice)
+            visited.Add(startNodeIndex); // Add the start node index to visited to begin with
+
+            while (queue.Count > 0) // While there are still nodes to visit
+            {
+                int currentIndex = queue.Dequeue();
+                MazeNode currentNode = currentMaze[currentIndex];
+
+                if (currentIndex == endNodeIndex) // If you've reached the ending, stop.
+                {
+                    return drawPath(parentMap, endNodeIndex);
+                }
+
+                // Time to check all four neighbours
+                if (!currentNode.getTopValue()) // If there is no top wall
+                {
+                    int neighborIndex = currentIndex - currentMazeData[0]; // Node one row up
+                    if (neighborIndex >= 0 && !visited.Contains(neighborIndex)) // If the neighbor exists and hasn't been visited
+                    {
+                        visited.Add(neighborIndex); // Mark neighbor as visited
+                        parentMap[neighborIndex] = currentIndex; // Set parent of neighbor to current node
+                        queue.Enqueue(neighborIndex); // Add neighbor to the queue (to iterate over this whole thing again)
+                    }
+                }
+
+                // Check the right wall (cell to the right)
+                if (!currentNode.getRightValue())
+                {
+                    int neighborIndex = currentIndex + 1;
+                    if (neighborIndex % currentMazeData[0] != 0 && !visited.Contains(neighborIndex)) // Make sure it's on the same row
+                    {
+                        visited.Add(neighborIndex);
+                        parentMap[neighborIndex] = currentIndex;
+                        queue.Enqueue(neighborIndex);
+                    }
+                }
+
+                // Check bottom wall (cell below)
+                if (!currentNode.getBottomValue())
+                {
+                    int neighborIndex = currentIndex + currentMazeData[0];
+                    if (neighborIndex < currentMazeData[3] && !visited.Contains(neighborIndex))
+                    {
+                        visited.Add(neighborIndex);
+                        parentMap[neighborIndex] = currentIndex;
+                        queue.Enqueue(neighborIndex);
+                    }
+                }
+
+                // Check left!
+                if (!currentNode.getLeftValue())
+                {
+                    int neighborIndex = currentIndex - 1;
+                    if ((neighborIndex + 1) % currentMazeData[0] != 0 && neighborIndex >= 0 && !visited.Contains(neighborIndex)) // Again, make sure it's on the same row
+                    { 
+                        visited.Add(neighborIndex);
+                        parentMap[neighborIndex] = currentIndex;
+                        queue.Enqueue(neighborIndex);
+                    }
+                }
+
+                // Unless selected node is the end node, there's bound to be something in the queue
+            }
+
+            return null; // No path found
+        }
+
+        // Retrace the path that we just made, but backwards
+        private List<int> drawPath(Dictionary<int, int> parentMap, int current)
+        {
+            foreach (nodalPictureBox box in mazePanel.Controls)
+            {
+                box.BackColor = DefaultBackColor;
+            } // Clear previous path colours
+            List<int> path = new List<int>();
+            while (current != -1) // -1 means the start node, so you've reached the beginning
+            {
+                path.Add(current); 
+                current = parentMap[current];
+            }
+            path.Reverse(); // Reverse to get from start to end, not end to start
+            outputLabel3.Text = "Path: " + string.Join(" -> ", path);
+            foreach (int index in path)
+            {
+                nodalPictureBox pathBox = (nodalPictureBox)mazePanel.Controls[index];
+                pathBox.BackColor = Color.FromArgb(155, 155, 255);
+            } // Colour in the path
+            outputLabel2.Text = $"Number of steps: {path.Count}";
+            return path;
+        }
         private void mazeBox_Paint(object sender, PaintEventArgs e)
         {
             nodalPictureBox currentBox = (nodalPictureBox)sender;
@@ -503,15 +696,13 @@ namespace Micromouse_demo1
             Shape shape = new Shape(node, currentMazeData[2], Color.Black, 40 / Math.Max(currentMazeData[0], currentMazeData[1]));
             shape.DrawShape(e.Graphics);
         }
-
         private void mazeBox_MouseClick(object sender, MouseEventArgs e)
         {
             nodalPictureBox currentBox = (nodalPictureBox)sender;
             MessageBox.Show(currentBox.MazeNode.NodeIndex.ToString());
             graphGenerationStarted = true;
-            graphGenerationAlgorithm(currentBox.MazeNode.NodeIndex);
+            BFS(currentBox.MazeNode.NodeIndex, currentMazeData[4]);
         }
-
         private void generateNewMaze_Click(object sender, EventArgs e)
         {
             clearAll();
@@ -519,11 +710,13 @@ namespace Micromouse_demo1
             {
                 MessageBox.Show("Please enter valid dimensions for the maze.");
                 return;
-            } else if (int.Parse(newMaze_width.Text) < 3 || int.Parse(newMaze_height.Text) < 3)
+            }
+            else if (int.Parse(newMaze_width.Text) < 3 || int.Parse(newMaze_height.Text) < 3)
             {
                 MessageBox.Show("Please enter dimensions greater than or equal to 3.");
                 return;
-            } else if (int.Parse(newMaze_width.Text) > 20 || int.Parse(newMaze_height.Text) > 20)
+            }
+            else if (int.Parse(newMaze_width.Text) > 20 || int.Parse(newMaze_height.Text) > 20)
             {
                 MessageBox.Show("Please enter dimensions less than or equal to 20.");
                 return;
@@ -531,10 +724,9 @@ namespace Micromouse_demo1
 
             int width = int.Parse(newMaze_width.Text);
             int height = int.Parse(newMaze_height.Text);
-            createEmptyMaze(width, height, 390 / Math.Max(width, height) );
+            createEmptyMaze(width, height, 390 / Math.Max(width, height));
             drawCurrentMaze();
         }
-
         private void prims_Button_Click(object sender, EventArgs e)
         {
             if (!primsAlgorithmStarted)
@@ -542,7 +734,8 @@ namespace Micromouse_demo1
                 primsAlgorithmStarted = true;
                 int node = random.Next(0, currentMazeData[3]);
                 primsAlgorithm(node);
-            } else
+            }
+            else
             {
                 MessageBox.Show("Prims Algorithm has already been started.");
                 return;
